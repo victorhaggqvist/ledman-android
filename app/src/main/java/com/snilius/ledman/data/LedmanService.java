35d1c8c714a448b2b4a074187b0a86403392aff5
Victor Häggqvist <victor@snilius.com>
2015-03-09 00:30:39 +0100

    fix: PUT not POST

--- app/src/main/java/com/snilius/ledman/data/LedmanService.java
+++ app/src/main/java/com/snilius/ledman/data/LedmanService.java
@@ -2,6 +2,7 @@ package com.snilius.ledman.data;
 
 import retrofit.http.GET;
 import retrofit.http.POST;
+import retrofit.http.PUT;
 import retrofit.http.Path;
 import retrofit.http.Query;
 
@@ -19,6 +20,6 @@ public interface LedmanService {
     @GET("/status")
     Status status();
 
-    @POST("/set/{color}")
+    @PUT("/set/{color}")
     String set(@Path("color") String color, @Query("level") String level);
 }
